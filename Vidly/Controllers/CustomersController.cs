2dbc127498193ff0b34bdfd8bd1a54786da14c44
jamesonatic <jamesonatic@users.noreply.github.com>
2021-01-10 20:10:05 +0100

    018 Added Customer pages

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id=1,Name="Customer 1"},
+                new Customer {Id=2,Name="Customer 2"}
+            };
+
+            return View(customers);
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
