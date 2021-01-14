498506ef00177e3691dbab8060e25f083622c43a
jamesonatic <jamesonatic@users.noreply.github.com>
2021-01-14 19:25:11 +0100

    34 Excercise 3.2 Added birthdate to the customer class and displayed in the details page

---
+++ Vidly/Migrations/202101141740034_AddedCustomerBirthdate.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCustomerBirthdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+            Sql("UPDATE Customers SET Birthdate = GETDATE()     WHERE Id = 1;");
+            Sql("UPDATE Customers SET Birthdate = '2000-12-31'  WHERE Id = 2;");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Birthdate");
+        }
+    }
+}
