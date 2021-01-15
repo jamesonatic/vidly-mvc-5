39cbc11c626c7e4243aebf91d30ed22aeccaca23
jamesonatic <jamesonatic@users.noreply.github.com>
2021-01-15 11:47:17 +0100

    39 Added new customer page

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,7 +12,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
